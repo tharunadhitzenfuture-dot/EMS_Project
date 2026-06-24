99ad67a9168f66bfc07e47829a7b56b1aef468ad
tharunadhitzenfuture-dot <tharunadhitzenfuture@gmail.com>
2026-06-24 16:21:26 +0530

    Commit

--- Backend/src/main/java/com/example/EMS/EmployeeController/LeaveController/LeavePolicyController.java
+++ Backend/src/main/java/com/example/EMS/EmployeeController/LeaveController/LeavePolicyController.java
@@ -1,12 +1,21 @@
 package com.example.EMS.EmployeeController.LeaveController;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.EMS.EmployeeEntity.LeaveEntity.LeavePolicy;
+import com.example.EMS.EmployeeRepository.LeaveRepository.LeavePolicyRepository;
 import com.example.EMS.EmployeeService.LeaveService.LeavePolicyService;
 
 @RestController
@@ -14,14 +23,12 @@ import com.example.EMS.EmployeeService.LeaveService.LeavePolicyService;
 public class LeavePolicyController {
 	
 	private LeavePolicyService leavePolicyService;
-	
+	private LeavePolicyRepository leavePolicyRepository;
 
-	public LeavePolicyController(LeavePolicyService leavePolicyService) {
-	
+	public LeavePolicyController(LeavePolicyService leavePolicyService, LeavePolicyRepository leavePolicyRepository) {
 		this.leavePolicyService = leavePolicyService;
+		this.leavePolicyRepository = leavePolicyRepository;
 	}
-
-
 
 	@PostMapping("/create")
 	public ResponseEntity<?> createPolicy(@RequestBody LeavePolicy request){
@@ -53,5 +60,54 @@ public class LeavePolicyController {
 		    
 		
 		    return leavePolicyService.createPolicy(request);
+	}
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<?> getAllPolicy(){
+		List<LeavePolicy>  lst = leavePolicyRepository.findAll();
+		
+		if(lst.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Leave policy list is empty");
+		}
+		
+		return ResponseEntity.ok(lst);
+	}
+	
+	@DeleteMapping("/deleteById/{id}")
+	public ResponseEntity<?> getAllPolicy(@PathVariable Long id){
+		if(id == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Please enter id");
+		}
+		Optional<LeavePolicy>  policy = leavePolicyRepository.findById(id);
+		
+		if(policy.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Leave policy is not found");
+		}
+		
+		return leavePolicyService.deleteById(id);
+	}
+	
+	@PatchMapping("/updateById/{id}")
+	public ResponseEntity<?> updateAllPolicy(@PathVariable Long id, @RequestBody LeavePolicy request){
+		if(id == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Please enter id");
+		}
+		Optional<LeavePolicy>  existing = leavePolicyRepository.findById(id);
+		
+		if(existing.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Leave policy is not found");
+		}
+		
+		LeavePolicy exist = existing.get();
+		
+		if(request.getTotalDays() != null) {
+			exist.setTotalDays(request.getTotalDays());
+		}
+		
+		if(request.getYear() != null) {
+			exist.setYear(request.getYear());
+		}
+
+		return leavePolicyService.updateById(id, exist);
 	}
 }
--- Backend/src/main/java/com/example/EMS/EmployeeService/LeaveService/LeavePolicyService.java
+++ Backend/src/main/java/com/example/EMS/EmployeeService/LeaveService/LeavePolicyService.java
@@ -13,6 +13,8 @@ import com.example.EMS.EmployeeRepository.EmpRepository;
 import com.example.EMS.EmployeeRepository.LeaveRepository.LeaveBalanceRepository;
 import com.example.EMS.EmployeeRepository.LeaveRepository.LeavePolicyRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class LeavePolicyService {
 	
@@ -93,5 +95,67 @@ public class LeavePolicyService {
 		
 		
 	}
+	
+	@Transactional
+	public ResponseEntity<?> deleteById(Long id){
+		leavePolicyRepo.deleteById(id);
+		return ResponseEntity.ok("Leave policy deleted");
+	}
+	
+	
+		public ResponseEntity<?> updateById(Long id, LeavePolicy request){
+		
+		//Optional<LeavePolicy> res = leavePolicyRepo.findByMonthAndYear(request.getMonth(), request.getYear());
+		List<Employee> employee = empRepo.findByProfessional_detailsProfessional_department(request.getDepartment().toString());
+		
+		if(employee.isEmpty()) {
+			return ResponseEntity.badRequest().body("Employee list for department "+request.getDepartment()+" is empty");
+		}
+
+		for(Employee emp: employee) {
+			
+			Optional<LeaveBalance> existing = leaveBalRepository.findByEmployeeAndYearAndTypeAndDepartment(emp,  request.getYear(), request.getType(), request.getDepartment());
+			if(existing.isPresent()) {
+				LeaveBalance balance = existing.get();
+				balance.setTotalDays(request.getTotalDays());
+				balance.setRemainingDays(request.getTotalDays());
+				balance.setType(request.getType());
+				balance.setDepartment(request.getDepartment());
+				leaveBalRepository.save(balance);
+				
+			}
+			else {
+				  LeaveBalance balance = new LeaveBalance();
+
+		            balance.setEmployee(emp);
+
+//		            balance.setMonth(
+//		                    request.getMonth()
+//		            );
+
+		            balance.setYear(
+		                    request.getYear()
+		            );
+
+		            balance.setTotalDays(
+		                    request.getTotalDays()
+		            );
+		            
+		            balance.setType(request.getType());
+		            balance.setRemainingDays(request.getTotalDays());
+		            balance.setUsedDays(0);
+		            balance.setDepartment(request.getDepartment());
+
+		            leaveBalRepository.save(balance);
+			}
+			
+		}
+		LeavePolicy save = leavePolicyRepo.save(request);
+
+		return ResponseEntity.ok(save);
+		
+		
+	}
+	
 
 }
