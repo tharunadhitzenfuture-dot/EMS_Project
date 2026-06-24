b1675daa06f4f8467022a1533fcf19bbaba99855
tharunadhitzenfuture-dot <tharunadhitzenfuture@gmail.com>
2026-06-24 09:50:43 +0530

    commit

--- Backend/src/main/java/com/example/EMS/EmployeeDTO/ResetPasswordDTO.java
+++ Backend/src/main/java/com/example/EMS/EmployeeDTO/ResetPasswordDTO.java
@@ -9,5 +9,25 @@ public class ResetPasswordDTO {
 	private String oldPassword;
 	private String password;
 	private String confirmPassword;
+	public String getOldPassword() {
+		return oldPassword;
+	}
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	
+	
 
 }
