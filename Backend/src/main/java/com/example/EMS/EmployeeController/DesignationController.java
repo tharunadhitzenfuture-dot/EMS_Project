cebf1c2388baae66fcf8c9486e0ff29e261e6d09
tharunadhitzenfuture-dot <tharunadhitzenfuture@gmail.com>
2026-07-02 16:37:43 +0530

    commit

---
+++ Backend/src/main/java/com/example/EMS/EmployeeController/DesignationController.java
@@ -0,0 +1,74 @@
+package com.example.EMS.EmployeeController;
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.EMS.EmployeeEntity.Designation;
+import com.example.EMS.EmployeeRepository.DesignationRepository;
+import com.example.EMS.EmployeeService.DesignationService;
+
+@RestController
+@RequestMapping("/api/designation")
+public class DesignationController {
+
+	private DesignationService service;
+	private DesignationRepository repository;
+	public DesignationController(DesignationService service, DesignationRepository repository) {
+		this.service = service;
+		this.repository = repository;
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<?> create(@RequestBody Designation request){
+		
+		if(request.getRole() == null || request.getRole().isBlank()) {
+			return ResponseEntity.badRequest().body("Please enter designation");
+		}		
+		Optional<Designation> designation =  repository.findByRole(request.getRole());		
+		if(designation.isPresent()) {
+			return ResponseEntity.badRequest().body("Designation already presented");
+		}		
+		return service.create(request);
+		
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<?> updateById(@PathVariable Long id,
+	                                    @RequestBody Designation request) {
+
+	    return service.updateById(id, request);
+	}
+
+	@GetMapping("/getAll")
+	public ResponseEntity<?> getAll() {
+
+	    return service.getAll();
+	}
+
+	@GetMapping("/getById/{id}")
+	public ResponseEntity<?> getById(@PathVariable Long id) {
+
+	    return service.getById(id);
+	}
+
+	@DeleteMapping("/deleteById/{id}")
+	public ResponseEntity<?> deleteById(@PathVariable Long id) {
+
+	    return service.deleteById(id);
+	}
+	
+	
+	
+	
+	
+	
+}
