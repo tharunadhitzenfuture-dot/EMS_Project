541930e520464b6fe384f3eaf7cf330ea5a24337
tharunadhitzenfuture-dot <tharunadhitzenfuture@gmail.com>
2026-07-03 14:19:10 +0530

    commit

--- Backend/src/main/java/com/example/EMS/EmployeeController/RoleAssignController.java
+++ Backend/src/main/java/com/example/EMS/EmployeeController/RoleAssignController.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,6 +66,19 @@ public class RoleAssignController {
 		}
 		
 		return ResponseEntity.ok(res.get());
+	}
+	
+	@DeleteMapping("/deleteById/{id}")
+	public ResponseEntity<?> getByRole(@PathVariable Long id){
+		Optional<RolesAssign>  res = repository.findById(id);
+		
+		if(res.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Role is not found.");
+		}
+		
+		repository.deleteById(id);
+		
+		return ResponseEntity.ok("Role deleted with id: "+id);
 	}
 
 }
